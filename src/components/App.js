bf9f44631e7fcb2b1f6e6cc3498b71a534768350
Casey Harding <casey.r.harding@gmail.com>
2021-11-02 13:00:20 -0400

    Finished through hobbies.

--- src/components/App.js
+++ src/components/App.js
@@ -50,6 +50,7 @@ export default function App() {
 
       {/* 👉 7- Render the FriendsList component */}
       {/* What prop/props does FriendsList need? */}
+      <FriendsList friends={friends} changeStatus={changeStatus} />
     </div>
   )
 }
--- src/components/Friend.js
+++ src/components/Friend.js
@@ -3,16 +3,19 @@ import PetsList from './PetsList'
 
 export default function Friend(props) {
   // 👉 1- What does a Friend need?
+  const { friend, changeStatus } = props;
 
   return (
     <div className='friend-friends container'>
       {/* 👉 2- Fix the JSX so it displays real info coming into the component */}
       <div className='friend-info'>
         <div >
-          <h3>Name: Jessica</h3>
-          <p>Age: 26</p>
+          <h3>Name: {friend.name}</h3>
+          <p>Age: {friend.age}</p>
 
-          <p>Married: yes <button>change</button></p>
+          <p>Married: {friend.married ? 'Married' : 'Single'} 
+            <button onClick={() => changeStatus(friend.id)}>change</button>
+          </p>
           <div>Likes:
             <ul>
               {/* 👉 3- Loop over the friend's hobbies and generate <li/> elements as you go */}
