dfcb09cd00212826f832424eb7ab2742d0238336
Roxanne Weber <marie.sankofa@gmail.com>
2021-11-03 21:27:10 -0500

    passed all values from App down to FriendsList down to Friend and added some JS variables and functions

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,50 @@
 // This is the top-level component
 // so we'll keep application state at this level.
 // 👉 1- Import the state hook!
-import React from 'react'
-import FriendsList from './FriendsList'
-import Search from './Search'
+import React, { useState } from 'react';
 // 👉 2- Import the dummy data that will power the application.
 // (Tomorrow we'll fetch the data from an API instead.)
-
+import friendsData from '../dummy-data/friends';
+import FriendsList from './FriendsList';
+import Search from './Search';
 
 export default function App() {
-  // 👉 3- Initialize a slice of state to keep track of the data
-  // using the dummy data as the initial value of the slice of state
+	// 👉 3- Initialize a slice of state to keep track of the data
+	// using the dummy data as the initial value of the slice of state
+	const [friends, setFriends] = useState(friendsData);
 
-  // 👉 4- Initialize a slice to keep track of the value of the search box
-  // using an empty string as the initial value of the slice
+	// 👉 4- Initialize a slice to keep track of the value of the search box
+	// using an empty string as the initial value of the slice
+	const [searchTerm, setSearchTerm] = useState('');
 
-  // 👉 5- Build a `changeStatus` function that takes an id and
-  // changes the `married` from true to false and viceversa
+	// 👉 5- Build a `changeStatus` function that takes an id and
+	// changes the `married` from true to false and viceversa
 
-  // STRETCH - Make a helper function that returns
-  // a filtered array of friends data (filtering by search term)
+	//* because we have to hang onto a variable to change it here, we first need to create another variable to hold the info we need
+	const changeStatus = (id) => {
+		//* we create a variable 'updatedFriends' and the value is equal to:
+		//* we map over all the elements in the friends data array, and we need access to all the individual object data, so we need the ...friends
+		const updatedFriends = friends.map((fr) => {
+			return fr.id === id ? { ...fr, married: !fr.married } : fr;
+		});
+		setFriends(updatedFriends);
+	};
 
-  return (
-    <div className='app-friends container'>
-      {/* 👉 6- Render the Search component */}
-      {/* STRETCH - Changes to the input should update the search term */}
+	// STRETCH - Make a helper function that returns
+	// a filtered array of friends data (filtering by search term)
 
-      {/* 👉 7- Render the FriendsList component */}
-      {/* What prop/props does FriendsList need? */}
-    </div>
-  )
+	return (
+		<div className='app-friends container'>
+			{/* 👉 6- Render the Search component */}
+			<Search />
+
+			{/* STRETCH - Changes to the input should update the search term */}
+
+			{/* 👉 7- Render the FriendsList component */}
+			{/* first check in FriendsList component to see what data we will need for it; then, add reference to those variables in our render statement so ensure App.js has access */}
+			<FriendsList friends={friends} changeStatus={changeStatus} />
+			{/* this statement will now 'pass down' these variables for use in the FriendsList component */}
+			{/* What prop/props does FriendsList need? */}
+		</div>
+	);
 }
--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React from 'react';
 // 👉 1- FriendsList renders several Friend components, import Friend
+import Friend from './Friend';
 
+// right now this function has access to friends array and changeStatus function within props
 export default function FriendsList(props) {
-  // 👉 2- What data does FriendsList need to do its job? Use destructuring
-  return (
-    <div className='list-friends container'>
-      {/* 👉 3- We need to loop over the data rendering a Friend as we go */}
-      {/* Each friend is going to need a `key` prop and also some other prop with data */}
-    </div>
-  )
+	console.log(props);
+
+	// 👉 2- What data does FriendsList need to do its job? Use destructuring
+	const { friends, changeStatus } = props; // now we have created two variables and added them to a props object
+
+	return (
+		<div className='list-friends container'>
+			{/* 👉 3- We need to loop over the data rendering a Friend as we go */}
+			{/* Each friend is going to need a `key` prop and also some other prop with data */}
+			{/* just like in the App component, we want our child Friend component to have access to the information it needs to process; below we map the entire array of friends, for each one we return a unique key (required) and all the friend object items plus the changeStatus */}
+			{friends.map((fr) => {
+				return (
+					<Friend friend={fr} changeStatus={changeStatus} key={fr.id} />
+				);
+			})}
+		</div>
+	);
 }
